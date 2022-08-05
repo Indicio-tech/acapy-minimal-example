3c5ea6a5f55c2985d823c48b033ac6f8e1417d0d
Colton Wolkins (Indicio work address) <colton@indicio.tech>
2022-08-05 10:53:37 -0600

    feat: Add example structure for class
    Explaining ideas as I go
    
    Signed-off-by: Colton Wolkins (Indicio work address) <colton@indicio.tech>

--- generator_lib/generator.py
+++ generator_lib/generator.py
@@ -1,6 +1,24 @@
 
+class Container:
+    pass
+
+class ACAPYContainer(Container):
+    pass
+
 
 class Generator:
 
-	def generate(self):
-		return "hi"
+    def __init__(self):
+        self.containers = list()
+        pass
+
+    def create_acapy(self) -> ACAPYContainer:
+        acapy = ACAPYContainer()
+        self.containers.append(acapy)
+        return acapy
+        pass
+
+    def generate(self):
+        for container in self.containers:
+            pass
+        return "hi"
