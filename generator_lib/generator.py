8b29f2bd9a3c0e8de6ac27bdfb8e9da815a5306a
Char Howland <char@indicio.tech>
2022-08-05 11:22:20 -0600

    feat: create ContainerConfig class
    Signed-off-by: Char Howland <char@indicio.tech>

--- generator_lib/generator.py
+++ generator_lib/generator.py
@@ -3,24 +3,35 @@ from typing import List
 class Container:
     pass
 
+class ContainerConfig:
+    pass
+
 class ACAPYContainer(Container):
-    def generate(slef) -> List[tuple]:
+    def generate(self) -> List[tuple]:
+        """
+        Returns: [(file name, file contents),]
+        """
         pass
 
+class PoetryConfig(ContainerConfig):
+    def generate(self):
+        pass
 
 class Generator:
 
     def __init__(self):
         self.containers = list()
-        pass
 
     def create_acapy(self) -> ACAPYContainer:
+        """ """
         acapy = ACAPYContainer()
         self.containers.append(acapy)
         return acapy
-        pass
 
     def generate(self):
+        """Create a docker-compose file from
+        list of containers
+        """
         for container in self.containers:
             pass
         return "hi"
