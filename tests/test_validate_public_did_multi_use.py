0c42a779bfdc8d59acd545b2f31c2940b7b424c4
Micah Peltier <micah6_8@yahoo.com>
2022-11-28 13:06:09 -0700

    feat: test public invite with multi use
    Signed-off-by: Micah Peltier <micah6_8@yahoo.com>

--- tests/test_validate_public_did_multi_use.py
+++ tests/test_validate_public_did_multi_use.py
@@ -4,13 +4,17 @@ from controller.controller import Controller
 from controller.models import (
     InvitationCreateRequest,
     InvitationRecord,
+    PingRequest,
 )
 from controller.protocols import _make_params, indy_anoncred_onboard, didexchange
+from controller.logging import logging_to_stdout
 
 
 @pytest.mark.asyncio
 async def test_multi_use_invite_with_public_did(alice: Controller, faber: Controller):
     """"""
+
+    logging_to_stdout()
     await indy_anoncred_onboard(faber)
 
     invite_record = await faber.post(
@@ -18,26 +22,38 @@ async def test_multi_use_invite_with_public_did(alice: Controller, faber: Contro
         json=InvitationCreateRequest(
             handshake_protocols=["https://didcomm.org/didexchange/1.0"],
             use_public_did=True,
-        ),  # pyright: ignore
+        ),
         params=_make_params(
-            # multi_use=True,
+            multi_use=True,
         ),
         response=InvitationRecord,
     )
     invite = invite_record.invitation
 
-    faber_conn, alice_conn = await didexchange(
-        inviter=faber, invitee=alice  # , invite=invite
-    )
+    for _ in range(2):
+        faber_conn, alice_conn = await didexchange(
+            inviter=faber, invitee=alice, invite=invite
+        )
 
-    assert faber_conn, alice_conn
+        assert faber_conn, alice_conn
 
-    print(faber_conn)
-    print(alice_conn)
+        print(faber_conn)
+        print(alice_conn)
 
-    ping = await faber.post(
-        f"/connections/{faber_conn.connection_id}/send-ping",
-    )
+        faber_conn_id = faber_conn.connection_id
+        alice_conn_id = alice_conn.connection_id
 
-    print(ping)
-    assert False
+        ping = await faber.post(
+            f"/connections/{faber_conn_id}/send-ping",
+            json=PingRequest(comment="Confirming active connection."),
+        )
+
+        assert "thread_id" in ping
+
+        await faber.delete(
+            f"/connections/{faber_conn_id}",
+        )
+
+        await alice.delete(
+            f"/connections/{alice_conn_id}",
+        )
