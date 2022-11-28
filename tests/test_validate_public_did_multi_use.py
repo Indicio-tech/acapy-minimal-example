1072fabeff35fae7f0cc266fffa19c7344cdf85a
Micah Peltier <micah6_8@yahoo.com>
2022-11-28 13:06:48 -0700

    feat: test metadata on public invite
    Signed-off-by: Micah Peltier <micah6_8@yahoo.com>

--- tests/test_validate_public_did_multi_use.py
+++ tests/test_validate_public_did_multi_use.py
@@ -2,6 +2,7 @@ import pytest
 
 from controller.controller import Controller
 from controller.models import (
+    ConnectionMetadata,
     InvitationCreateRequest,
     InvitationRecord,
     PingRequest,
@@ -57,3 +58,43 @@ async def test_multi_use_invite_with_public_did(alice: Controller, faber: Contro
         await alice.delete(
             f"/connections/{alice_conn_id}",
         )
+
+
+@pytest.mark.asyncio
+async def test_metadata_with_public_did(alice: Controller, bob: Controller):
+
+    logging_to_stdout()
+
+    await indy_anoncred_onboard(bob)
+
+    invite_record = await bob.post(
+        "/out-of-band/create-invitation",
+        json=InvitationCreateRequest(
+            handshake_protocols=["https://didcomm.org/didexchange/1.0"],
+            use_public_did=True,
+            metadata={"stuff": "and things", "keys": "and values"},
+        ),
+        response=InvitationRecord,
+    )
+    invite = invite_record.invitation
+
+    bob_conn, alice_conn = await didexchange(inviter=bob, invitee=alice, invite=invite)
+
+    bob_conn_id = bob_conn.connection_id
+
+    ping = await bob.post(
+        f"/connections/{bob_conn_id}/send-ping",
+        json=PingRequest(comment="Confirming active connection."),
+    )
+
+    assert "thread_id" in ping
+
+    bob_metadata = await bob.get(
+        f"/connections/{bob_conn_id}/metadata",
+        response=ConnectionMetadata,
+    )
+
+    print(bob_metadata)
+
+    assert bob_metadata.results["stuff"] == "and things"
+    assert bob_metadata.results["keys"] == "and values"
