c4eed87b6d1c46ab871a8243e675b2859be87c28
Alex Walker <alex.walker@indicio.tech>
2022-12-12 21:30:14 +0100

    fix: fix minimal_example.py
    Signed-off-by: Alex Walker <alex.walker@indicio.tech>

--- minimal_example.py
+++ minimal_example.py
@@ -1,5 +1,4 @@
 """Minimal reproducible example script.
-
 This script is for you to use to reproduce a bug or demonstrate a feature.
 """
 
@@ -8,7 +7,7 @@ from os import getenv
 import time
 from typing import List
 
-from .models import V10PresentationExchangeList
+from controller.models import V10PresentationExchangeList
 
 from controller import Controller
 from controller.logging import logging_to_stdout
@@ -103,13 +102,11 @@ async def main():
         # Revoke credential v1
         await indy_anoncreds_revoke(
             alice,
-            v10_credential_exchange_object=alice_cred_ex,
+            cred_ex=alice_cred_ex,
             holder_connection_id=alice_cred_ex.connection_id,
             notify=True,
         )
-        await indy_anoncreds_publish_revocation(
-            alice, v10_credential_exchange_object=alice_cred_ex
-        )
+        await indy_anoncreds_publish_revocation(alice, cred_ex=alice_cred_ex)
         # TODO: Make this into a helper in protocols.py?
         await bob.record(topic="revocation-notification")
 
@@ -234,7 +231,140 @@ async def main():
         # Presentation summary
         for i, pres in enumerate(presentations.results):
             print(summary(pres))
+        return presentations
 
+        # Version 2.0
+        # Revoke credential v2
+        await indy_anoncreds_revoke(
+            alice,
+            cred_ex=alice_cred_ex_v2,
+            holder_connection_id=alice_cred_ex_v2.connection_id,
+            notify=True,
+            notify_version="v2_0",
+        )
+        await indy_anoncreds_publish_revocation(alice, cred_ex=alice_cred_ex_v2)
+        # Record again
+        await bob.record(topic="revocation-notification")
+
+        # Request proof from holder again after revoking
+        before_revoking_time = non_revoked_time
+        revoked_time = int(time.time())
+        (
+            bob_pres_ex_v2_entirely_after_int,
+            alice_pres_ex_v2_entirely_after_int,
+        ) = await indy_present_proof_v1(
+            bob,
+            alice,
+            bob_conn.connection_id,
+            alice_conn.connection_id,
+            requested_attributes=[
+                {
+                    "name": "firstname",
+                    "restrictions": [
+                        {"cred_def_id": cred_def.credential_definition_id}
+                    ],
+                }
+            ],
+            non_revoked={"from": revoked_time - 1, "to": revoked_time},
+        )
+
+        # Request proof from holder again after revoking,
+        # using the interval before cred revoked
+        # (non_revoked interval/when cred was valid)
+        revoked_time = int(time.time())
+        (
+            bob_pres_ex_v2_after_revo_using_before_interval,
+            alice_pres_ex_v2_after_revo_using_before_interval,
+        ) = await indy_present_proof_v2(
+            bob,
+            alice,
+            bob_conn.connection_id,
+            alice_conn.connection_id,
+            requested_attributes=[
+                {
+                    "name": "firstname",
+                    "restrictions": [
+                        {"cred_def_id": cred_def.credential_definition_id}
+                    ],
+                }
+            ],
+            non_revoked={"from": before_revoking_time, "to": before_revoking_time},
+        )
+
+        # Request proof, no interval
+        (
+            bob_pres_ex_v2_after_revo_no_interval,
+            alice_pres_ex_v2_after_revo_no_interval,
+        ) = await indy_present_proof_v2(
+            bob,
+            alice,
+            bob_conn.connection_id,
+            alice_conn.connection_id,
+            requested_attributes=[
+                {
+                    "name": "firstname",
+                    "restrictions": [
+                        {"cred_def_id": cred_def.credential_definition_id}
+                    ],
+                }
+            ],
+        )
+
+        # Request proof, using invalid/revoked interval but using
+        # local non_revoked override (in requsted attrs)
+        # ("LOCAL"-->requested attrs)
+        (
+            bob_pres_ex_v2_after_revo_global_invalid_local_override_w_valid,
+            alice_pres_ex_v2_after_revo_global_invalid_local_override_w_valid,
+        ) = await indy_present_proof_v2(
+            bob,
+            alice,
+            bob_conn.connection_id,
+            alice_conn.connection_id,
+            requested_attributes=[
+                {
+                    "name": "firstname",
+                    "restrictions": [
+                        {"cred_def_id": cred_def.credential_definition_id}
+                    ],
+                    "non_revoked": {
+                        "from": before_revoking_time - 1,
+                        "to": before_revoking_time,
+                    },
+                }
+            ],
+            non_revoked={"from": revoked_time - 1, "to": revoked_time},
+        )
+
+        # Request proof, just local invalid interval
+        (
+            bob_pres_ex_v2_after_revo_local_invalid,
+            alice_pres_ex_v2_local_invalid,
+        ) = await indy_present_proof_v2(
+            bob,
+            alice,
+            bob_conn.connection_id,
+            alice_conn.connection_id,
+            requested_attributes=[
+                {
+                    "name": "firstname",
+                    "restrictions": [
+                        {"cred_def_id": cred_def.credential_definition_id}
+                    ],
+                    "non_revoked": {
+                        "from": revoked_time,
+                        "to": revoked_time,
+                    },
+                }
+            ],
+        )
+
+        # Query presentations
+        presentations = await alice.get("/present-proof-2.0/records")
+
+        # Presentation summary
+        for i, pres in enumerate(presentations.results):
+            print(summary(pres))
         return presentations
 
 
