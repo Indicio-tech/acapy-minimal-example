4568dcf4cf7696f82ff71e52559edac2f5034976
Alex Walker <alex.walker@indicio.tech>
2023-01-17 16:33:10 +0100

    feat: add scratch work
    Signed-off-by: Alex Walker <alex.walker@indicio.tech>

--- minimal_example.py
+++ minimal_example.py
@@ -99,6 +99,130 @@ async def main():
         )
         print(alice_pres_ex.json(by_alias=True, indent=2))
 
+        # Revoke credential v1
+        await indy_anoncreds_revoke(
+            alice,
+            cred_ex=alice_cred_ex,
+            holder_connection_id=alice_cred_ex.connection_id,
+            notify=True,
+        )
+        await indy_anoncreds_publish_revocation(alice, cred_ex=alice_cred_ex)
+        # TODO: Make this into a helper in protocols.py?
+        await bob.record(topic="revocation-notification")
+
+        # Request proof from holder again after revoking
+        before_revoking_time = non_revoked_time
+        revoked_time = int(time.time())
+        (
+            bob_pres_ex_v1_entirely_after_int,
+            alice_pres_ex_v1_entirely_after_int,
+        ) = await indy_present_proof_v1(
+            bob,
+            alice,
+            bob_conn.connection_id,
+            alice_conn.connection_id,
+            requested_attributes=[
+                {
+                    "name": "firstname",
+                    "restrictions": [
+                        {"cred_def_id": cred_def.credential_definition_id}
+                    ],
+                }
+            ],
+            non_revoked={"from": revoked_time - 1, "to": revoked_time},
+        )
+
+        # Request proof from holder again after revoking,
+        # using the interval before cred revoked
+        # (non_revoked interval/when cred was valid)
+        revoked_time = int(time.time())
+        (
+            bob_pres_ex_v1_after_revo_using_before_interval,
+            alice_pres_ex_v1_after_revo_using_before_interval,
+        ) = await indy_present_proof_v1(
+            bob,
+            alice,
+            bob_conn.connection_id,
+            alice_conn.connection_id,
+            requested_attributes=[
+                {
+                    "name": "firstname",
+                    "restrictions": [
+                        {"cred_def_id": cred_def.credential_definition_id}
+                    ],
+                }
+            ],
+            non_revoked={"from": before_revoking_time, "to": before_revoking_time},
+        )
+
+        # Request proof, no interval
+        (
+            bob_pres_ex_v1_after_revo_no_interval,
+            alice_pres_ex_v1_after_revo_no_interval,
+        ) = await indy_present_proof_v1(
+            bob,
+            alice,
+            bob_conn.connection_id,
+            alice_conn.connection_id,
+            requested_attributes=[
+                {
+                    "name": "firstname",
+                    "restrictions": [
+                        {"cred_def_id": cred_def.credential_definition_id}
+                    ],
+                }
+            ],
+        )
+
+        # Request proof, using invalid/revoked interval but using
+        # local non_revoked override (in requsted attrs)
+        # ("LOCAL"-->requested attrs)
+        (
+            bob_pres_ex_v1_after_revo_global_invalid_local_override_w_valid,
+            alice_pres_ex_v1_after_revo_global_invalid_local_override_w_valid,
+        ) = await indy_present_proof_v1(
+            bob,
+            alice,
+            bob_conn.connection_id,
+            alice_conn.connection_id,
+            requested_attributes=[
+                {
+                    "name": "firstname",
+                    "restrictions": [
+                        {"cred_def_id": cred_def.credential_definition_id}
+                    ],
+                    "non_revoked": {
+                        "from": before_revoking_time - 1,
+                        "to": before_revoking_time,
+                    },
+                }
+            ],
+            non_revoked={"from": revoked_time - 1, "to": revoked_time},
+        )
+
+        # Request proof, just local invalid interval
+        (
+            bob_pres_ex_v1_after_revo_local_invalid,
+            alice_pres_ex_v1__local_invalid,
+        ) = await indy_present_proof_v1(
+            bob,
+            alice,
+            bob_conn.connection_id,
+            alice_conn.connection_id,
+            requested_attributes=[
+                {
+                    "name": "firstname",
+                    "restrictions": [
+                        {"cred_def_id": cred_def.credential_definition_id}
+                    ],
+                    "non_revoked": {
+                        "from": revoked_time,
+                        "to": revoked_time,
+                    },
+                }
+            ],
+        )
+
         # Query presentations
         presentations = await alice.get(
             "/present-proof/records", response=V10PresentationExchangeList
@@ -109,6 +233,131 @@ async def main():
             print(summary(pres))
         return presentations
 
+        # Version 2.0
+        # Revoke credential v2
+        await indy_anoncreds_revoke(
+            alice,
+            cred_ex=alice_cred_ex_v2,
+            holder_connection_id=alice_cred_ex_v2.connection_id,
+            notify=True,
+            notify_version="v2_0",
+        )
+        await indy_anoncreds_publish_revocation(alice, cred_ex=alice_cred_ex_v2)
+        # Record again
+        await bob.record(topic="revocation-notification")
+
+        # Request proof from holder again after revoking
+        before_revoking_time = non_revoked_time
+        revoked_time = int(time.time())
+        (
+            bob_pres_ex_v2_entirely_after_int,
+            alice_pres_ex_v2_entirely_after_int,
+        ) = await indy_present_proof_v1(
+            bob,
+            alice,
+            bob_conn.connection_id,
+            alice_conn.connection_id,
+            requested_attributes=[
+                {
+                    "name": "firstname",
+                    "restrictions": [
+                        {"cred_def_id": cred_def.credential_definition_id}
+                    ],
+                }
+            ],
+            non_revoked={"from": revoked_time - 1, "to": revoked_time},
+        )
+
+        # Request proof from holder again after revoking,
+        # using the interval before cred revoked
+        # (non_revoked interval/when cred was valid)
+        revoked_time = int(time.time())
+        (
+            bob_pres_ex_v2_after_revo_using_before_interval,
+            alice_pres_ex_v2_after_revo_using_before_interval,
+        ) = await indy_present_proof_v2(
+            bob,
+            alice,
+            bob_conn.connection_id,
+            alice_conn.connection_id,
+            requested_attributes=[
+                {
+                    "name": "firstname",
+                    "restrictions": [
+                        {"cred_def_id": cred_def.credential_definition_id}
+                    ],
+                }
+            ],
+            non_revoked={"from": before_revoking_time, "to": before_revoking_time},
+        )
+
+        # Request proof, no interval
+        (
+            bob_pres_ex_v2_after_revo_no_interval,
+            alice_pres_ex_v2_after_revo_no_interval,
+        ) = await indy_present_proof_v2(
+            bob,
+            alice,
+            bob_conn.connection_id,
+            alice_conn.connection_id,
+            requested_attributes=[
+                {
+                    "name": "firstname",
+                    "restrictions": [
+                        {"cred_def_id": cred_def.credential_definition_id}
+                    ],
+                }
+            ],
+        )
+
+        # Request proof, using invalid/revoked interval but using
+        # local non_revoked override (in requsted attrs)
+        # ("LOCAL"-->requested attrs)
+        (
+            bob_pres_ex_v2_after_revo_global_invalid_local_override_w_valid,
+            alice_pres_ex_v2_after_revo_global_invalid_local_override_w_valid,
+        ) = await indy_present_proof_v2(
+            bob,
+            alice,
+            bob_conn.connection_id,
+            alice_conn.connection_id,
+            requested_attributes=[
+                {
+                    "name": "firstname",
+                    "restrictions": [
+                        {"cred_def_id": cred_def.credential_definition_id}
+                    ],
+                    "non_revoked": {
+                        "from": before_revoking_time - 1,
+                        "to": before_revoking_time,
+                    },
+                }
+            ],
+            non_revoked={"from": revoked_time - 1, "to": revoked_time},
+        )
+
+        # Request proof, just local invalid interval
+        (
+            bob_pres_ex_v2_after_revo_local_invalid,
+            alice_pres_ex_v2_local_invalid,
+        ) = await indy_present_proof_v2(
+            bob,
+            alice,
+            bob_conn.connection_id,
+            alice_conn.connection_id,
+            requested_attributes=[
+                {
+                    "name": "firstname",
+                    "restrictions": [
+                        {"cred_def_id": cred_def.credential_definition_id}
+                    ],
+                    "non_revoked": {
+                        "from": revoked_time,
+                        "to": revoked_time,
+                    },
+                }
+            ],
+        )
 
         # Query presentations
         presentations = await alice.get("/present-proof-2.0/records")
