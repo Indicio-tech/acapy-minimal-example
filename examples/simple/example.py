21ec5d95c3cbdb1dd17f4d8c5cb982a76d567c01
Daniel Bluhm <dbluhm@pm.me>
2023-08-03 09:16:22 -0400

    fix: simple example
    Signed-off-by: Daniel Bluhm <dbluhm@pm.me>

--- examples/simple/example.py
+++ examples/simple/example.py
@@ -3,9 +3,11 @@
 This script is for you to use to reproduce a bug or demonstrate a feature.
 """
 
+import asyncio
 from os import getenv
 
 from controller import Controller
+from controller.logging import logging_to_stdout
 from controller.protocols import didexchange
 
 ALICE = getenv("ALICE", "http://alice:3001")
@@ -16,3 +18,8 @@ async def main():
     """Test Controller protocols."""
     async with Controller(base_url=ALICE) as alice, Controller(base_url=BOB) as bob:
         await didexchange(alice, bob)
+
+
+if __name__ == "__main__":
+    logging_to_stdout()
+    asyncio.run(main())
