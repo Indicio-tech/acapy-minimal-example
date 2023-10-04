036ebea289514d2ead8ce4c9e2cb6c89ff07dba2
Daniel Bluhm <dbluhm@pm.me>
2023-10-04 17:20:02 -0400

    feat: add mediation example
    Signed-off-by: Daniel Bluhm <dbluhm@pm.me>

---
+++ examples/mediation/example.py
@@ -0,0 +1,35 @@
+"""Minimal reproducible example script.
+
+This script is for you to use to reproduce a bug or demonstrate a feature.
+"""
+
+import asyncio
+from os import getenv
+
+from controller import Controller
+from controller.logging import logging_to_stdout
+from controller.protocols import didexchange, request_mediation_v1
+
+ALICE = getenv("ALICE", "http://alice:3001")
+BOB = getenv("BOB", "http://bob:3001")
+MEDIATOR = getenv("MEDIATOR", "http://mediator:3001")
+
+
+async def main():
+    """Test Controller protocols."""
+    alice = Controller(base_url=ALICE)
+    bob = Controller(base_url=BOB)
+    mediator = Controller(base_url=MEDIATOR)
+
+    async with alice, bob, mediator:
+        ma, am = await didexchange(mediator, alice)
+        mam, amm = await request_mediation_v1(
+            mediator, alice, ma.connection_id, am.connection_id
+        )
+        await alice.put(f"/mediation/{amm.mediation_id}/default-mediator")
+        await didexchange(alice, bob)
+
+
+if __name__ == "__main__":
+    logging_to_stdout()
+    asyncio.run(main())
