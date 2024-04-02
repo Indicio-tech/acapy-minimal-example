2fb3510e94195ff1a53004645339aef543ae82fb
Daniel Bluhm <dbluhm@pm.me>
2024-04-02 16:29:11 -0400

    test: oob webhook
    Signed-off-by: Daniel Bluhm <dbluhm@pm.me>

--- examples/simple/example.py
+++ examples/simple/example.py
@@ -8,7 +8,7 @@ from os import getenv
 
 from controller import Controller
 from controller.logging import logging_to_stdout
-from controller.protocols import connection, didexchange
+from controller.protocols import didexchange, oob_invitation
 
 ALICE = getenv("ALICE", "http://alice:3001")
 BOB = getenv("BOB", "http://bob:3001")
@@ -17,8 +17,8 @@ BOB = getenv("BOB", "http://bob:3001")
 async def main():
     """Test Controller protocols."""
     async with Controller(base_url=ALICE) as alice, Controller(base_url=BOB) as bob:
-        await connection(alice, bob)
-        await didexchange(alice, bob)
+        invite = await oob_invitation(alice, multi_use=True)
+        alice_conn, bob_conn = await didexchange(alice, bob, invite=invite)
 
 
 if __name__ == "__main__":
